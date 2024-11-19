550d433b3c4abe217fa5e7fa33c51754121c382f
Ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-11-19 21:49:24 +0200

    add credit card widget

--- lib/features/presentation/checkout/views/widget/paymnet_details_body.dart
+++ lib/features/presentation/checkout/views/widget/paymnet_details_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:paymentapp/features/presentation/checkout/views/widget/card_contaner_list_view.dart';
 
 class PaymnetDetailsBody extends StatelessWidget {
@@ -13,9 +14,39 @@ class PaymnetDetailsBody extends StatelessWidget {
           const SizedBox(
             height: 18,
           ),
-          CardContainerListView()
+          CardContainerListView(),
+          CustomCreditCard(),
         ],
       ),
+    );
+  }
+}
+
+class CustomCreditCard extends StatefulWidget {
+  const CustomCreditCard({super.key});
+
+  @override
+  State<CustomCreditCard> createState() => _CustomCreditCardState();
+}
+
+class _CustomCreditCardState extends State<CustomCreditCard> {
+  String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
+
+  bool showBackView = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CreditCardWidget(
+          cardNumber: cardNumber,
+          expiryDate: expiryDate,
+          cardHolderName: cardHolderName,
+          cvvCode: cvvCode,
+          showBackView: showBackView,
+          onCreditCardWidgetChange: (p0) {},
+        )
+      ],
     );
   }
 }
