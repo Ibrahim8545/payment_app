a031071080dfe7bc3c5f0204798f2dd98ea49ef0
Ibrahim8545 <ibrahimmostafa122020@gmail.com>
2024-11-20 15:00:08 +0200

    add form

--- lib/features/presentation/checkout/views/widget/paymnet_details_body.dart
+++ lib/features/presentation/checkout/views/widget/paymnet_details_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:paymentapp/features/presentation/checkout/views/widget/card_contaner_list_view.dart';
+import 'package:paymentapp/features/presentation/checkout/views/widget/custom_credit_card.dart';
 
 class PaymnetDetailsBody extends StatelessWidget {
   const PaymnetDetailsBody({super.key});
@@ -18,50 +18,6 @@ class PaymnetDetailsBody extends StatelessWidget {
           CustomCreditCard(),
         ],
       ),
-    );
-  }
-}
-
-class CustomCreditCard extends StatefulWidget {
-  const CustomCreditCard({super.key});
-
-  @override
-  State<CustomCreditCard> createState() => _CustomCreditCardState();
-}
-
-class _CustomCreditCardState extends State<CustomCreditCard> {
-  String cardNumber = '', expiryDate = '', cardHolderName = '', cvvCode = '';
-
-  bool showBackView = false;
-  final GlobalKey<FormState> formKey = GlobalKey();
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        CreditCardWidget(
-          cardNumber: cardNumber,
-          expiryDate: expiryDate,
-          cardHolderName: cardHolderName,
-          cvvCode: cvvCode,
-          showBackView: showBackView,
-          isHolderNameVisible: true,
-          onCreditCardWidgetChange: (p0) {},
-        ),
-        CreditCardForm(
-            cardNumber: cardNumber,
-            expiryDate: expiryDate,
-            cardHolderName: cardHolderName,
-            cvvCode: cvvCode,
-            onCreditCardModelChange: (creditCardModel) {
-              cardNumber = creditCardModel.cardNumber;
-              expiryDate = creditCardModel.expiryDate;
-              cardHolderName = creditCardModel.cardHolderName;
-              cvvCode = creditCardModel.cvvCode;
-              showBackView = creditCardModel.isCvvFocused;
-              setState(() {});
-            },
-            formKey: formKey)
-      ],
     );
   }
 }
